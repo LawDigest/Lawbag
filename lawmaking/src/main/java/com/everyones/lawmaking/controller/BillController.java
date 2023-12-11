a2a8e06f0de70c81c86d094540311e051fd9343c
ganjisriver <ganjisriver18@gmail.com>
2023-12-11 21:21:50 +0900

    #feat/#4/메인피드-1차-개발 우선 메인피드에 최신의안으로 나오게 페이지네이션 적용 메인피드 로직을 다양한 변수에 의해 나오게 고치는 것은 추후에 추가할 예정

--- lawmaking/src/main/java/com/everyones/lawmaking/controller/BillController.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/controller/BillController.java
@@ -4,6 +4,7 @@ import com.everyones.lawmaking.common.dto.BaseResponse;
 import com.everyones.lawmaking.common.dto.response.MainFeedBillResponse;
 import com.everyones.lawmaking.service.BillService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,11 +23,13 @@ public class BillController {
     private final BillService billService;
 
     @GetMapping("/mainfeed")
-    public ResponseEntity<?> getNext3Bills(@RequestParam(required = false) String lastBillId,
-                                                              @RequestParam(required = false) LocalDateTime lastProposeDate) {
+    public ResponseEntity<Map<String, Object>> getNext3Bills(
+            @RequestParam(name = "page", required = true) int page,
+            @RequestParam(name = "size", required = true) int size,
+            Pageable pageable) {
         try {
-            var result = billService.getNext3Bills(lastBillId, lastProposeDate);
-            var resp = BaseResponse.generateSuccessResponse(Collections.singletonList(result));
+            var result = billService.getNext3Bills(page, pageable);
+            var resp = BaseResponse.generateSuccessResponse(result);
             return new ResponseEntity<>(resp, HttpStatus.OK);
         } catch (Exception e) {
             var resp = BaseResponse.generateErrorResponse(false, 500, e.getMessage(), null);
