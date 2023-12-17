b9fdbdaeea18ed9e07e53f3a31acfd04f39b1d00
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2023-12-18 03:55:47 +0900

    이미지 주소 적용

--- lawmaking/src/main/java/com/everyones/lawmaking/config/SecurityConfig.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig implements WebMvcConfigurer { // WebMvcConfigurer 인터페이스 구현(implements)
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("resources/**") // 핸들러 추가
+        registry.addResourceHandler("/v1/images/**") // 핸들러 추가
                 .addResourceLocations("classpath:/static/") // 클래스패스 설정시 끝에 꼭 / 넣어주자.
                 .setCachePeriod(20); // 초단위
     }
