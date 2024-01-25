4e76e34ae5bd3b4f086a9507a6c87c71bb2d8cf5
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-01-25 20:28:16 +0900

    Swagger Update

--- lawmaking/src/main/java/com/everyones/lawmaking/controller/PartyController.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/controller/PartyController.java
@@ -3,7 +3,9 @@ package com.everyones.lawmaking.controller;
 import com.everyones.lawmaking.common.dto.response.PartyDetailResponse;
 import com.everyones.lawmaking.global.BaseResponse;
 import com.everyones.lawmaking.service.PartyService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +16,14 @@ public class PartyController {
     private final PartyService partyService;
 
     @GetMapping("/detail/{party_id}")
-    public BaseResponse<PartyDetailResponse> getPartyDetail(@PathVariable("party_id") long partyId,
-                                                            @RequestParam("stage") String stage, // "represent" or "public"
-                                                            @RequestParam("page") int page,
-                                                            @RequestParam("size") int size,
-                                                            Pageable pageable) {
-
+    public BaseResponse<PartyDetailResponse>
+    getPartyDetail(@PathVariable("party_id") long partyId,
+                   @RequestParam("stage") String stage,
+                   @Parameter(example = "0", description = "스크롤할 때마다 page값을 0에서 1씩 늘려주면 됩니다.")
+                   @RequestParam(name = "page", required = true) int page,
+                   @Parameter(example = "3", description = "한번에 가져올 데이터 크기를 의미합니다.")
+                   @RequestParam(name = "size", required = true) int size) {
+        var pageable = PageRequest.of(page, size);
         var result = partyService.getPartyDetail(page, pageable, partyId, stage);
         return BaseResponse.ok(result);
 
---
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/SwaggerConstants.java
@@ -0,0 +1,10 @@
+package com.everyones.lawmaking.global;
+
+public class SwaggerConstants {
+    public static final String EXAMPLE_ERROR_500_CONTENT = (
+            "{\"status\": 500, " +
+                    "\"message\": \"Internal server error\", " +
+                    "\"data\": null, " +
+                    "\"success\": false}"
+    );
+}
