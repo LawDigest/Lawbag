c557b2f8455d15e8005ba7ef581fb8f97facca60
Gotprgmer <gofiction95@gmail.com>
2024-02-27 19:06:28 +0900

    Util 파일 추가

---
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/util/HeaderUtil.java
@@ -0,0 +1,26 @@
+package com.everyones.lawmaking.global.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HeaderUtil {
+    private static final String HEADER_AUTHORIZATION = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+
+    private HeaderUtil(){
+        throw new IllegalStateException("HeaderUtil class");
+    }
+
+    public static String getAccessToken(HttpServletRequest request) {
+        String headerValue = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (headerValue == null) {
+            return null;
+        }
+
+        if (headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
+}
