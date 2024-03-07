6352401b3785cacec95557d4a1f39f40fced5b87
강정훈 Kang Jeonghun <109566855+ganjisriver@users.noreply.github.com>
2024-03-07 20:34:07 +0900

    Fix(#Cors-Error) CORS 에러 수정 (#72)
    Cors-Error 수정

--- lawmaking/src/main/java/com/everyones/lawmaking/global/config/CorsConfig.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.everyones.lawmaking.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -10,13 +11,14 @@ import java.util.List;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         var corsConfigSource = new UrlBasedCorsConfigurationSource();
 
         var corsConfig = new CorsConfiguration();
         corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.setAllowedMethods(List.of("*"));
-        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000", "http://52.79.63.140:3000"));
         corsConfig.setAllowCredentials(true);
         corsConfig.setMaxAge(3600L);
 
