9b3493ffcdbdfa9befe420d9379e6ebf4c1081af
Gotprgmer <gofiction95@gmail.com>
2024-03-22 22:07:13 +0900

    사용자 조회 AuthInfo arg 타입 변경

--- lawmaking/src/main/java/com/everyones/lawmaking/repository/UserRepository.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByAuthInfo_Id(long authInfoId);
+    Optional<User> findByAuthInfo_Id(Long authInfoId);
 
 }
