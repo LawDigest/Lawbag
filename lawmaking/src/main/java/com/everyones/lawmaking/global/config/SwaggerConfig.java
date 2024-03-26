f7b838c3f2557f1b2cf9b45a60a9f5f9f0ac17d7
Gotprgmer <gofiction95@gmail.com>
2024-03-27 02:54:31 +0900

    Swagger에서 Api 요청 시 토큰 자동으로 삽입 기능 추가

--- lawmaking/src/main/java/com/everyones/lawmaking/global/config/SwaggerConfig.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/config/SwaggerConfig.java
@@ -3,17 +3,30 @@ package com.everyones.lawmaking.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer-token");
+
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                .addSecuritySchemes("bearer-token",securityScheme))
+                .security(Collections.singletonList(securityRequirement))
                 .info(apiInfo());
     }
 
