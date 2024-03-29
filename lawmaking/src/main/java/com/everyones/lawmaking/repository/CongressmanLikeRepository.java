ae6c4500f67ce0715e564f01a0a1c976197440d0
Gotprgmer <gofiction95@gmail.com>
2024-03-30 00:59:45 +0900

    FIX : UserRepository로 구독 사용자관련 쿼리 메소드 이전

--- lawmaking/src/main/java/com/everyones/lawmaking/repository/CongressmanLikeRepository.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/repository/CongressmanLikeRepository.java
@@ -1,13 +1,11 @@
 package com.everyones.lawmaking.repository;
 
 import com.everyones.lawmaking.domain.entity.CongressManLike;
-import com.everyones.lawmaking.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,9 +15,7 @@ public interface CongressmanLikeRepository extends JpaRepository<CongressManLike
             "WHERE cl.user.id = :userId AND cl.congressman.id = :congressmanId")
     Optional<CongressManLike> findByUserIdAndCongressmanId(@Param("userId")long userId, @Param("congressmanId")String congressmanId);
 
-    @Query("SELECT cl.user FROM CongressManLike cl " +
-            "WHERE cl.congressman.id = :congressmanId")
-    List<User> findAllByCongressmanId(@Param("congressmanId") String congressmanId);
+
 
 
 
--- lawmaking/src/main/java/com/everyones/lawmaking/repository/UserRepository.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/repository/UserRepository.java
@@ -2,13 +2,24 @@ package com.everyones.lawmaking.repository;
 
 import com.everyones.lawmaking.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByAuthInfo_Id(Long authInfoId);
+
+    @Query("SELECT cl.user FROM CongressManLike cl " +
+            "WHERE cl.congressman.id = :congressmanId")
+    List<User> findAllByCongressmanId(@Param("congressmanId") String congressmanId);
+
+    @Query("select bl.user from BillLike bl " +
+            "where bl.bill.id = :billId")
+    List<User> findAllByBillId(@Param("billId") String billId);
 
 }
