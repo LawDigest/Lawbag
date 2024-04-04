872e89c991e76a964da951dc7f311ee07d989505
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-04-04 13:18:41 +0900

    법안에 briefSummary 필드 추가 및 로직 변경

--- lawmaking/src/main/java/com/everyones/lawmaking/common/dto/BillDetailInfo.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/BillDetailInfo.java
@@ -14,14 +14,14 @@ public class BillDetailInfo extends BillInfoDto{
     private String stage;
     private String[] keyword;
 
-    public BillDetailInfo(String billId, String billName, LocalDate proposeDate, String summary, String gptSummary, int viewCount, int billLikeCount, String stage, String keyword) {
-        super(billId, billName, proposeDate, summary, gptSummary, viewCount, billLikeCount);
+    public BillDetailInfo(String billId, String billName, LocalDate proposeDate, String summary, String gptSummary, int viewCount, int billLikeCount, String stage, String keyword, String briefSummary) {
+        super(billId, billName, proposeDate, summary, gptSummary, viewCount, billLikeCount, briefSummary);
         this.stage = stage;
         this.keyword = keyword.split(", ");
     }
 
     public static BillDetailInfo from(Bill bill) {
-        return new BillDetailInfo(bill.getId(), bill.getBillName(), bill.getProposeDate(), bill.getSummary(), bill.getGptSummary(), bill.getViewCount(), bill.getLikeCount(), bill.getStage(), bill.getKeyword());
+        return new BillDetailInfo(bill.getId(), bill.getBillName(), bill.getProposeDate(), bill.getSummary(), bill.getGptSummary(), bill.getViewCount(), bill.getLikeCount(), bill.getStage(), bill.getKeyword(), bill.getBriefSummary());
     }
 
 }
