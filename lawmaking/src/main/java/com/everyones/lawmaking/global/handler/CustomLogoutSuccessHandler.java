073339cde15fdc82c1b37dc95c6cc2fa6f9a3075
Gotprgmer <gofiction95@gmail.com>
2024-04-06 00:36:50 +0900

    Fix : 로그아웃 성공 시, 200상태코드 부여

--- lawmaking/src/main/java/com/everyones/lawmaking/global/handler/CustomLogoutSuccessHandler.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/handler/CustomLogoutSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.everyones.lawmaking.global.handler;
 
 import com.everyones.lawmaking.global.BaseResponse;
+import com.nimbusds.jose.shaded.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,13 +11,17 @@ import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuc
 import java.io.IOException;
 
 public class CustomLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
-    public final static String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
 
 
 
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        BaseResponse.ok("logout successed");
+        BaseResponse<String> responseMessage = BaseResponse.ok("Logout succeeded");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("application/json");
+        response.getWriter().write(new Gson().toJson(responseMessage));
+        response.getWriter().flush();
+        response.setStatus(200);
     }
 }
