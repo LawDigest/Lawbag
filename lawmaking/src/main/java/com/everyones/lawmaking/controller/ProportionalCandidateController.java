578f283257e153ca8c88c7328962389428b1dc99
Gotprgmer <gofiction95@gmail.com>
2024-04-07 17:36:23 +0900

    Feat : 비례대표 정당 정보 조회 api

--- lawmaking/src/main/java/com/everyones/lawmaking/controller/ProportionalCandidateController.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/controller/ProportionalCandidateController.java
@@ -2,6 +2,7 @@ package com.everyones.lawmaking.controller;
 
 import com.everyones.lawmaking.common.dto.response.ProportionalCandidateListResponse;
 import com.everyones.lawmaking.common.dto.response.ProportionalPartyImageListResponse;
+import com.everyones.lawmaking.common.dto.response.ProportionalPartyResponse;
 import com.everyones.lawmaking.facade.Facade;
 import com.everyones.lawmaking.global.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -70,13 +71,34 @@ public class ProportionalCandidateController {
                     )}
             ),
     })
-    @GetMapping("/proportional_candidate/party_logo")
+    @GetMapping("/party_logo")
     public BaseResponse<ProportionalPartyImageListResponse> getProPartyImage(
             @Parameter(example = "0", description = "스크롤할 때마다 page값을 0에서 1씩 늘려주면 됩니다.")
             @RequestParam(name = "page") int page){
         var pageable = PageRequest.of(page, 5);
 
         var result = facade.getProPartyImage(pageable);
+        return BaseResponse.ok(result);
+    }
+
+    @Operation(summary = "비례대표 정당 정보 조회 ", description = "비례대표 정당 정보 조회하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버 오류 (문제 지속시 BE팀 문의)",
+                    content = {@Content(
+                            mediaType = "application/json;charset=UTF-8",
+                            schema = @Schema(implementation = BaseResponse.class),
+                            examples = @ExampleObject(value = EXAMPLE_ERROR_500_CONTENT)
+                    )}
+            ),
+    })
+    @GetMapping("/party_info")
+    public BaseResponse<ProportionalPartyResponse> getProPartyImage(
+            @Parameter(example = "1", description = "정당 Id")
+            @RequestParam("party_id") long partyId){
+        var result = facade.getProPartyInfo(partyId);
         return BaseResponse.ok(result);
     }
 }
