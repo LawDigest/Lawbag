5f410d416c5a428e2fd39cb65a0ae7e8b46d3280
Gotprgmer <gofiction95@gmail.com>
2024-04-12 20:03:28 +0900

    Fix : RestTemplateUtil 이름 바꿈

--- lawmaking/src/main/java/com/everyones/lawmaking/global/config/RestTemplateConfig.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/config/RestTemplateConfig.java
@@ -1,11 +1,11 @@
-package com.everyones.lawmaking.global.util;
+package com.everyones.lawmaking.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateUtil {
+public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate()  {
         return new RestTemplate();
