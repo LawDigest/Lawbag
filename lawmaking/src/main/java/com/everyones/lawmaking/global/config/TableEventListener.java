6e45e6770fbbb3a1c41250e9aab19f14210c577f
MinJunKim <55742497+GotPrgmer@users.noreply.github.com>
2024-07-01 00:51:25 +0900

    알림 스레드 실행 원복 (#236)
    * 알림 스레드 실행 원복
    
    * 알림 스레드 실행 원복

--- lawmaking/src/main/java/com/everyones/lawmaking/global/config/TableEventListener.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/config/TableEventListener.java
@@ -53,10 +53,10 @@ public class TableEventListener {
 
     private final NotificationCreator notificationCreator;
 
-    @Bean
-    public ExecutorService executorService() {
-        return Executors.newFixedThreadPool(10); // 스레드 풀 크기 10으로 설정
-    }
+//    @Bean
+//    public ExecutorService executorService() {
+//        return Executors.newFixedThreadPool(10); // 스레드 풀 크기 10으로 설정
+//    }
 
     /**
      * 테이블별 (컬럼네임,컬럼 인덱스) 정보 가져오는 메서드
@@ -259,13 +259,16 @@ public class TableEventListener {
                 }
             }
         });
-//        executorService().submit(() -> {
-//            try {
-//                logClient.connect();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
+        Thread thread = new Thread(() -> {
+            try {
+                logClient.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        thread.start();
 
 
         return logClient;
