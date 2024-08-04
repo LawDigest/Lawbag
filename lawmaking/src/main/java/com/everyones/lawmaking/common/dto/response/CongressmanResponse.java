7d3f761bd614e3a14d4595079e2eb5001808e6c7
강정훈 Kang Jeonghun <109566855+ganjisriver@users.noreply.github.com>
2024-08-04 17:42:51 +0900

    의원 정보 api 수정 (#266)

--- lawmaking/src/main/java/com/everyones/lawmaking/common/dto/response/CongressmanResponse.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/response/CongressmanResponse.java
@@ -1,12 +1,12 @@
 package com.everyones.lawmaking.common.dto.response;
 
 import com.everyones.lawmaking.domain.entity.Congressman;
+import com.everyones.lawmaking.global.util.DateUtil;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 @Builder
 @Data
 @AllArgsConstructor
@@ -15,20 +15,25 @@ public class CongressmanResponse {
     // Congressman 관련 필드들
     private String congressmanId;
     private String congressmanName;
-
-    // Party 관련 필드들
-    private Long partyId;
-    private String partyName;
-    private String partyImageUrl;
     private String electSort;
     private String district;
     private String commits;
     private String elected;
     private String homepage;
-    private int representCount;
-    private int publicCount;
+    private String gender;
+    private Integer age;
+    private String email;
+    private String office;
+    private Integer representCount;
+    private Integer publicCount;
     private String congressmanImageUrl;
     private Boolean likeChecked;
+    // Party 관련 필드들
+    private Long partyId;
+    private String partyName;
+    private String partyImageUrl;
+
+
 
 
     public static CongressmanResponse from(Congressman congressman)  {
@@ -46,6 +51,10 @@ public class CongressmanResponse {
                 .representCount(congressman.getRepresentCount())
                 .publicCount(congressman.getPublicCount())
                 .congressmanImageUrl(congressman.getCongressmanImageUrl())
+                .age(DateUtil.calculateAge(congressman.getCongressmanAge()))
+                .email(congressman.getEmail())
+                .gender(congressman.getSex())
+                .office(congressman.getCongressmanOffice())
                 .likeChecked(false)
                 .build();
     }
