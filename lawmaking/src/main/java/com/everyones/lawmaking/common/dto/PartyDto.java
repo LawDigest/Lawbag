f890e8e5cf563d835a640831351dd90cf3413041
강정훈 Kang Jeonghun <109566855+ganjisriver@users.noreply.github.com>
2024-08-14 15:00:42 +0900

    Fix(#Similar-BIll) 다른 개정안 보기 수정 (#283)
    * mysql 드라이버 라이브러리 교체
    
    * SimilarBill 로직 교체

--- lawmaking/src/main/java/com/everyones/lawmaking/common/dto/PartyDto.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/PartyDto.java
@@ -1,16 +1,30 @@
 package com.everyones.lawmaking.common.dto;
 
+import com.everyones.lawmaking.domain.entity.Party;
+import com.everyones.lawmaking.domain.entity.RepresentativeProposer;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
+@Builder
 @AllArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PartyDto {
     private long partyId;
     private String partyName;
     private String partyImageUrl;
-    private int partyCount;
+
+    public static PartyDto from(Party party) {
+        return PartyDto.builder()
+                .partyId(party.getId())
+                .partyName(party.getName())
+                .partyImageUrl(party.getPartyImageUrl())
+                .build();
+    }
+
 }
--- lawmaking/src/main/java/com/everyones/lawmaking/common/dto/SimilarBill.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/SimilarBill.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Builder
 @AllArgsConstructor
@@ -13,12 +16,29 @@ public class SimilarBill {
     @NotNull
     private String billId;
     @NotNull
+    private String billBriefSummary;
+    @NotNull
     private String billName;
+    @NotNull
+    private String billProposers;
+    private String billStage;
+    @NotNull
+    private List<PartyDto> party;
 
     public static SimilarBill from(Bill bill) {
+        List<PartyDto> partyList = new ArrayList<>();
+        var representProposers = bill.getRepresentativeProposer();
+        for (var representProposer : representProposers) {
+            partyList.add(PartyDto.from(representProposer.getCongressman().getParty()));
+        }
+
         return SimilarBill.builder()
                 .billId(bill.getId())
-                .billName(bill.getBriefSummary() + " (" + bill.getProposers() + ")")
+                .billBriefSummary(bill.getBriefSummary())
+                .billName(bill.getBillName())
+                .billProposers(bill.getProposers())
+                .billStage(bill.getStage())
+                .party(partyList)
                 .build();
     }
 
