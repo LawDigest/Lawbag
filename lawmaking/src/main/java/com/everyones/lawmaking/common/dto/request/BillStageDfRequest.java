2f6ee44879879c6eab40473e6b6bc65bdd9826f7
Gotprgmer <gofiction95@gmail.com>
2024-08-15 12:57:34 +0900

    법안 타임라인 데이터 삽입에 필요한 클래스 생성

--- lawmaking/src/main/java/com/everyones/lawmaking/common/dto/request/BillStageDfRequest.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/request/BillStageDfRequest.java
@@ -3,16 +3,23 @@ package com.everyones.lawmaking.common.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BillStageDfRequest {
-    private int age;
 
-    private Long billNumber;
+    private String billId;
 
     private String stage;
 
     private String committee;
+
+    private String actStatusValue;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private LocalDate statusUpdateDate;
 }
---
+++ lawmaking/src/main/java/com/everyones/lawmaking/domain/entity/BillTimeline.java
@@ -0,0 +1,54 @@
+package com.everyones.lawmaking.domain.entity;
+
+import com.everyones.lawmaking.common.dto.request.BillStageDfRequest;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BillTimeline {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bill_timeline_id")
+    private long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "bill_id")
+    private Bill bill;
+
+    @Column(name="act_status")
+    private String actStatus;
+
+    @Column(name="bill_timeline_stage")
+    private String billTimelineStage;
+
+    @Column(name = "bill_timeline_committee")
+    private String billTimelineCommittee;
+
+
+    @Column(name = "status_update_date")
+    private LocalDate statusUpdateDate;
+
+    public static BillTimeline of (Bill foundBill, BillStageDfRequest billStageDfRequest){
+        var stageChangeDate = billStageDfRequest.getStatusUpdateDate();
+        System.out.println(billStageDfRequest.getStage());
+        return BillTimeline.builder()
+                .bill(foundBill)
+                .actStatus(billStageDfRequest.getActStatusValue())
+                .billTimelineCommittee(billStageDfRequest.getCommittee())
+                .statusUpdateDate(stageChangeDate)
+                .billTimelineStage(billStageDfRequest.getStage())
+                .build();
+    }
+
+
+}
