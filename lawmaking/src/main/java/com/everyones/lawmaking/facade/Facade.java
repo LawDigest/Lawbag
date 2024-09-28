17da09146713499b622fc585a3c24fc63512eb11
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2024-09-28 20:26:53 +0900

    BillOutlineDTO 정당 정보 추가

--- lawmaking/src/main/java/com/everyones/lawmaking/facade/Facade.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/facade/Facade.java
@@ -20,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
