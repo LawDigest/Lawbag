c4d15abff31e6b4f36f945f8b652d5937de4c80c
Gotprgmer <gofiction95@gmail.com>
2024-11-06 15:02:56 +0900

    카카오 unlink, 엑세스토큰 재발급 로직

---
+++ lawmaking/src/main/java/com/everyones/lawmaking/common/dto/response/KakaoAccessTokenRefreshResponse.java
@@ -0,0 +1,21 @@
+package com.everyones.lawmaking.common.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class KakaoAccessTokenRefreshResponse {
+    private String accessToken;
+    private String tokenType;
+    private Integer expiresIn;
+    private String refreshToken;
+    private Integer refreshTokenExpiresIn;
+}
