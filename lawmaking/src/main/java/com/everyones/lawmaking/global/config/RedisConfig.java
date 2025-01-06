bb3268586c1b21108e7135161fa45b41a217a333
Gotprgmer <gofiction95@gmail.com>
2025-01-07 00:39:28 +0900

    fix : @EnableRedisRepositories 삭제

--- lawmaking/src/main/java/com/everyones/lawmaking/global/config/RedisConfig.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
