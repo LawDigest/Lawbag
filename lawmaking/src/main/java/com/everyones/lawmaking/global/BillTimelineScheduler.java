edc286e8bd9dab3749c4ac7c1480858776178cd5
강정훈 Kang Jeonghun <ganjisriver18@gmail.com>
2025-04-12 19:31:18 +0900

    법안 타임라인 스케줄링

--- lawmaking/src/main/java/com/everyones/lawmaking/global/BillTimelineScheduler.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/BillTimelineScheduler.java
@@ -14,20 +14,21 @@ public class BillTimelineScheduler {
     private final BillTimelineRepository billTimelineRepository;
     private final Facade facade;
 
-    @Scheduled(cron = "0 0/10 8-19 * * MON-FRI")
+    @Scheduled(cron = "0 0/30 8-19 * * MON-FRI")
     public void refreshTimelineCache() {
         LocalDate today = LocalDate.now();
         String TIMELINE_KEY = "timelineQueue";
         String todayKey = TIMELINE_KEY + today;
         LocalDate recentUpdateDate = billTimelineRepository.findTopByOrderByStatusUpdateDateDesc();
+        // 오래된 데이터 제거
+        LocalDate tenDaysAgo = today.minusDays(7);
+        String oldestKey = TIMELINE_KEY + tenDaysAgo;
+        redisTemplate.delete(oldestKey);
+
         // 업데이트 할 필요가 없다면 return
         if (!today.equals(recentUpdateDate) || Boolean.TRUE.equals(redisTemplate.hasKey(todayKey))) {
             return;
         }
-        // 오래된 데이터 제거
-        LocalDate tenDaysAgo = today.minusDays(10);
-        String oldestKey = TIMELINE_KEY + tenDaysAgo;
-        redisTemplate.delete(oldestKey);
 
         // 오늘 데이터 추가
         BillTimelineResponse billTimelineResponse = facade.getTimeline(today);
