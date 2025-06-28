8c9b55b512a8128d49182b664ebc1f7c91c2c976
ganjisriver <109566855+ganjisriver@users.noreply.github.com>
2025-06-28 16:24:31 +0900

    Feat(#Social-Login-Naver) 네이버 소셜로그인 연동 (#427)
    * naver 로그인 로직 완료
    
    * 네이버 환경 변수 추가

--- lawmaking/src/main/java/com/everyones/lawmaking/global/auth/socialInfo/OAuth2UserInfoFactory.java
+++ lawmaking/src/main/java/com/everyones/lawmaking/global/auth/socialInfo/OAuth2UserInfoFactory.java
@@ -7,12 +7,15 @@ import java.util.Map;
 import java.util.Objects;
 
 import static com.everyones.lawmaking.domain.entity.Provider.KAKAO;
+import static com.everyones.lawmaking.domain.entity.Provider.NAVER;
 
 @Slf4j
 public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(Provider provider, Map<String, Object> attributes) {
         if (Objects.requireNonNull(provider) == KAKAO) {
             return new KakaoOAuth2UserInfo(attributes);
+        } else if (Objects.requireNonNull(provider) == NAVER) {
+            return new NaverOAuth2UserInfo(attributes);
         }
         throw new IllegalArgumentException("유효하지 않은 Provider Type 입니다.");
     }
